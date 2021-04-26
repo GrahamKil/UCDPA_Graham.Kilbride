09ce74e49325c9f59a69378b6a9678d32f0081a8
GrahamKil <g_kibride@yahoo.ie>
2021-04-26 18:35:19 +0100

    Adding CVS file and malu data

--- Learning Outcomes.py
+++ Learning Outcomes.py
@@ -1,2 +1,29 @@
 # Creating Project Repo
-# hello
+
+# importing CSV file
+import pandas as pd
+
+netflix_df = pd.read_csv('netflix_titles.csv', index_col='show_id')
+
+print(netflix_df.isna().sum())
+
+
+# removing coloum with the most missing values
+netflix_df1 = netflix_df.drop('director', axis=1)
+
+
+# dropping duplicates
+netflix_df1.drop_duplicates()
+
+print(netflix_df1.isna().sum())
+
+
+# slicing a Movies and TV
+moives_tv = netflix_df1.loc[:, :'type']
+
+release_year = netflix_df1.loc[:, ['country','release_year','listed_in']]
+
+# Iterate through DataFrame
+for moives, type in netflix_df1.iterrows():
+    print(pd.value_counts(''))
+
